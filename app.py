98e8f9b1666ae983afa1f34ea105b216bb192e61
kurachi <matheuskurachi6@gmail.com>
2026-04-16 08:53:08 -0300

    add initial post data structure to app.py

--- app.py
+++ app.py
@@ -4,3 +4,19 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static", name="static"))
 templates = Jinjacle2Templates(directory="templates")
+
+post_memoria = [
+
+{
+
+    "id": 1,    
+    "titulo": "Meu primeiro post",
+    "resumo": "Um resumo sobre o post",
+    "conteudo": "Conteúdo completo..",
+    "autor": "Professor Carlos"
+
+
+
+}
+
+                ]
