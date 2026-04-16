a4f6b98ae5ef4578c11fb0b18c488ca2914c1557
kurachi <matheuskurachi6@gmail.com>
2026-04-16 08:46:42 -0300

    initialize FastAPI and JINJA2 setup for web forum and add requirements for project dependencies

--- functions.py
+++ functions.py
@@ -1,1 +1,8 @@
-print("functions")
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Form
+from fastapi.responses import RedirectResponse
+
+# pip install -r requirements.txt - para baixar as libs necessárias de uma vez só!#
